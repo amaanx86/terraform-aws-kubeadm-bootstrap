c236197bd05d67246a59fcd3d0367bb593a134ce
Amaan Ul Haq Siddiqui <amaanulhaq.s@outlook.com>
2026-06-04 01:42:24 +0500

    feat: add Terraform provider, backend, and data sources
    Signed-off-by: Amaan Ul Haq Siddiqui <amaanulhaq.s@outlook.com>

---
+++ terraform/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-*-${var.ubuntu_version}-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
